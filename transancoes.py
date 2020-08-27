9ef6aa676db213b81717b4c38892c948a105d24c
Gabriel Joshua <gjoshuacalixto@outlook.com>
2020-08-26 21:25:45 -0300

    Registro de Saldo
    Está implementado as funções que resguarda a impossibilidade de gastro duplo na carteira

--- transancoes.py
+++ transancoes.py
@@ -18,6 +18,34 @@ class Input:
         self.outputIndex = None
         self.signature = None
 
+class UnspentOutput:
+    def __init__(self, outputId, outputIndex, address, amount):
+        self.outputID = outputID
+        self.outputIndex = outputIndex
+        self.address = address
+        self.amount = amount
+
+class UnspentOutputs:
+    def __init__(self):
+        self.__listUtxo = []
+
+    def updateListUtxo(self, list):
+        self.__init__ = list
+
+    def newUnspentOutputs(self, transactions):
+        list = []
+        for transaction in transactions:
+            for inpt in transaction.Input:
+                utxo = UnspentOutput(transaction.id, inpt.outputId, inpt.outputIndex, inpt.address, inpt.amount)
+                list.append(utxo)
+
+        self.updateListUtxo(list)
+
+def findUnspentOutput (outputId, outputIndex, listUnspentOutputs):
+    for utxo in listUnspentOutputs:
+        if utxo.outputId == outputId and utxo.outputIndex == outputIndex:
+            return True
+
 def idTransaction(transaction):
     inputContents = ""
     outputContents = ""
