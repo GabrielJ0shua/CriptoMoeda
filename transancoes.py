d5f59803554bd59be35363d349c93e018c6b74db
Gabriel Joshua <gjoshuacalixto@outlook.com>
2020-08-28 21:31:30 -0300

    Implementação para trabalho de transação
    Implementação de pagamento para trabalho de transação

--- transancoes.py
+++ transancoes.py
@@ -2,6 +2,8 @@ import hashlib
 import binascii
 from ecdsa import SigningKey, NIST384p
 
+COINBASE = 100
+
 class Transaction:
     def __init__(self):
         self.id = None
@@ -81,3 +83,16 @@ def validatingTransactionId(id, transaction):
         return True
     else:
         return False
+
+def validatingCoinbase(transaction, index):
+    if idTransaction(transaction) != transaction.id:
+        return False
+    elif len(transaction.inputs) != 1
+        return False
+    elif transaction.inputs[0].outputIndex != index:
+        return False
+    elif len(transaction.outputs) != 1:
+        return False
+    elif transaction.outputs[0].amount != COINBASE:
+        return False
+    return True
